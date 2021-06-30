dfc7d1f945dd94fd084170b56381dff565080e72
Pavel Karpy <carpawell@nspcc.ru>
2021-06-30 22:14:36 +0300

    [#15] monitor/metrics: Refactor metrics initialization
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- pkg/monitor/metrics.go
+++ pkg/monitor/metrics.go
@@ -5,42 +5,52 @@ import (
 )
 
 var (
-	countriesPresent = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Subsystem: "neofs_net_monitor",
-		Name:      "netmap",
-		Help:      "Countries where NeoFS storage nodes are located",
-	},
+	countriesPresent = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: "neofs_net_monitor",
+			Name:      "netmap",
+			Help:      "Countries where NeoFS storage nodes are located",
+		},
 		[]string{
 			"country",
-		})
+		},
+	)
 
-	epochNumber = prometheus.NewGauge(prometheus.GaugeOpts{
-		Subsystem: "neofs_net_monitor",
-		Name:      "epoch",
-		Help:      "Epoch number of NeoFS network",
-	})
+	epochNumber = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Subsystem: "neofs_net_monitor",
+			Name:      "epoch",
+			Help:      "Epoch number of NeoFS network",
+		},
+	)
 
-	innerRingBalances = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Subsystem: "neofs_net_monitor",
-		Name:      "ir_balance",
-		Help:      "Side chain GAS amount of inner ring nodes",
-	},
+	innerRingBalances = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: "neofs_net_monitor",
+			Name:      "ir_balance",
+			Help:      "Side chain GAS amount of inner ring nodes",
+		},
 		[]string{
 			"key",
-		})
+		},
+	)
 
-	storageNodeBalances = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Subsystem: "neofs_net_monitor",
-		Name:      "sn_balance",
-		Help:      "Side chain GAS amount of storage nodes",
-	},
+	storageNodeBalances = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: "neofs_net_monitor",
+			Name:      "sn_balance",
+			Help:      "Side chain GAS amount of storage nodes",
+		},
 		[]string{
 			"key",
-		})
+		},
+	)
 
-	proxyBalance = prometheus.NewGauge(prometheus.GaugeOpts{
-		Subsystem: "neofs_net_monitor",
-		Name:      "proxy_balance",
-		Help:      "Side chain GAS amount of proxy contract",
-	})
+	proxyBalance = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Subsystem: "neofs_net_monitor",
+			Name:      "proxy_balance",
+			Help:      "Side chain GAS amount of proxy contract",
+		},
+	)
 )
