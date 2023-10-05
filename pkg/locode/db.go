7fca38497b5a5223599f598978aa899092615738
Evgenii Baidakov <evgenii@nspcc.io>
2023-10-05 16:45:48 +0400

    *: Use any instead of interface{}
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- pkg/locode/db.go
+++ pkg/locode/db.go
@@ -45,7 +45,7 @@ type DB struct {
 
 const invalidPrmValFmt = "invalid parameter %s (%T):%v"
 
-func panicOnPrmValue(n string, v interface{}) {
+func panicOnPrmValue(n string, v any) {
 	panic(fmt.Sprintf(invalidPrmValFmt, n, v, v))
 }
 
