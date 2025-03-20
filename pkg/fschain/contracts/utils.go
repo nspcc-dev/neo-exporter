0e76f543eaea586a49ecb649551ba81f895c201f
Evgenii Baidakov <evgenii@nspcc.io>
2025-03-20 09:39:12 +0400

    go.mod: Upgrade github.com/nspcc-dev/neofs-sdk-go v1.0.0-rc.12 => v1.0.0-rc.13
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- pkg/fschain/contracts/utils.go
+++ pkg/fschain/contracts/utils.go
@@ -12,7 +12,11 @@ import (
 func parseContractNodes(data []*rcpnetmap.NetmapNode) ([]*netmap.NodeInfo, error) {
 	nodes := make([]*netmap.NodeInfo, 0, len(data))
 	for _, d := range data {
-		var nodeInfoV2 v2netmap.NodeInfo
+		var (
+			nodeInfoV2 v2netmap.NodeInfo
+			nodeInfo   netmap.NodeInfo
+		)
+
 		if err := nodeInfoV2.Unmarshal(d.BLOB); err != nil {
 			return nil, fmt.Errorf("can't unmarshal peer info: %w", err)
 		}
@@ -20,18 +24,26 @@ func parseContractNodes(data []*rcpnetmap.NetmapNode) ([]*netmap.NodeInfo, error
 		switch d.State.Int64() {
 		case int64(nodestate.Online):
 			nodeInfoV2.SetState(v2netmap.Online)
+			nodeInfo.SetOnline()
 		case int64(nodestate.Offline):
 			nodeInfoV2.SetState(v2netmap.Offline)
+			nodeInfo.SetOffline()
 		case int64(nodestate.Maintenance):
 			nodeInfoV2.SetState(v2netmap.Maintenance)
+			nodeInfo.SetMaintenance()
 		default:
 			nodeInfoV2.SetState(v2netmap.UnspecifiedState)
 		}
 
-		var nodeInfo netmap.NodeInfo
-		if err := nodeInfo.ReadFromV2(nodeInfoV2); err != nil {
-			return nil, fmt.Errorf("failed to read node info from v2: %w", err)
+		var (
+			attrs [][2]string
+		)
+
+		for _, attribute := range nodeInfoV2.GetAttributes() {
+			attrs = append(attrs, [2]string{attribute.GetKey(), attribute.GetValue()})
 		}
+
+		nodeInfo.SetAttributes(attrs)
 
 		nodes = append(nodes, &nodeInfo)
 	}
